aee0e204b9401e5c4a67cd283ec5c8843c57d825
Victor <victor.ardila0205@gmail.com>
2024-10-08 13:19:03 -0500

    finished

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import { BrowserRouter, Link, Routes, Route } from 'react-router-dom';
+import React, { useEffect, useState } from 'react';
 
 import './pages/components/CssFiles/briefcase.css';
 import './pages/components/CssFiles/btnsRes.css';
@@ -16,25 +17,42 @@ import Contact from './pages/Contact';
 import Briefcase from './pages/BriefCase';
 
 function App() {
+  const [isMenuOpen, setMenuOpen] = useState(false); // Estado para manejar la apertura del menú
+
+  const toggleMenu = () => {
+    setMenuOpen((prev) => !prev);
+  };
+
+  useEffect(() => {
+    const handleResize = () => {
+      if (window.innerWidth > 960) {
+        setMenuOpen(false); // Cierra el menú en pantallas grandes
+      }
+    };
+
+    window.addEventListener('resize', handleResize);
+    return () => {
+      window.removeEventListener('resize', handleResize);
+    };
+  }, []);
+
   return (
     <BrowserRouter>
-      <div id="menu">☰</div>
-      <div id="navContent">
+      <div id="menu" onClick={toggleMenu}>☰</div>
+      <div id="navContent" style={{ display: isMenuOpen || window.innerWidth > 960 ? 'block' : 'none' }}>
         <ul>
-          <li><Link id="actualPag" to="/">Curriculum</Link></li> {/* Cambiado a / para que sea la página de inicio */}
+          <li><Link id="actualPag" to="/">Curriculum</Link></li>
           <li><Link to="/certificados">Certificados</Link></li>
-          <li><Link to="/mybriefcase">Contacto</Link></li> {/* Cambiado a /mybriefcase */}
-          <li><Link to="/portafolio">Portafolio</Link></li> {/* Cambiado a /portafolio */}
+          <li><Link to="/mybriefcase">Contacto</Link></li>
+          <li><Link to="/portafolio">Portafolio</Link></li>
         </ul>
-        </div>
-        
-        {/* Área de contenido donde se renderizan las páginas */}
-          <Routes>
-            <Route path="/" element={<Index/>} /> {/* Página de inicio */}
-            <Route path="/certificados" element={<Cer />} /> {/* Ruta para certificados */}
-            <Route path="/mybriefcase" element={<Contact />} /> {/* Ejemplo de página de contacto */}
-            <Route path="/portafolio" element={<Briefcase />} /> {/* Ejemplo de página de portafolio */}
-          </Routes>
+      </div>
+      <Routes>
+        <Route path="/" element={<Index />} />
+        <Route path="/certificados" element={<Cer />} />
+        <Route path="/mybriefcase" element={<Contact />} />
+        <Route path="/portafolio" element={<Briefcase />} />
+      </Routes>
     </BrowserRouter>
   );
 }
